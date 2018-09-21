8e4bfc6bb7633e07936963b3dd47656a11c5f21d
Navdeep Singh <navdeep.er@gmail.com>
2018-09-21 16:05:49 +0530

    Array.from solution added

--- 005-fizzbuzz/index.js
+++ 005-fizzbuzz/index.js
@@ -1,5 +1,5 @@
 // Solution 1
-const fizzBuzz = (number) => {
+const fizzBuzz1 = (number) => {
   let print = '';
   for(let i = 1; i <= number; i++) {
     // Is the number mulpiple of 3 and 5
@@ -18,6 +18,18 @@ const fizzBuzz = (number) => {
   }
 }
 
-//const fizzBuzz = fizzBuzz;
+// Solution 2
+//Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+const range = (count) => Array.from({length: count}, (v,i) => i + 1); // Helper function
+const fizzBuzz2 = (number) => {
+  for (i of range(number)) {
+    if (i % (3 * 5) === 0) console.log('fizzbuzz');
+    else if (i % 3 === 0) console.log('fizz');
+    else if (i % 5 === 0) console.log('buzz');
+    else console.log(i);
+  }
+}
+
+const fizzBuzz = fizzBuzz2;
 
 module.exports = fizzBuzz;
