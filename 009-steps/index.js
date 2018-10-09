ef274bd0d802e54c17f9ceec2a6a9be197698025
Navdeep Singh <navdeep.er@gmail.com>
2018-10-09 12:24:30 +0530

    Recursion solution and Code Refactor

--- 009-steps/index.js
+++ 009-steps/index.js
@@ -1,5 +1,5 @@
 // Solution 1
-const steps = (n) => {
+const steps1 = (n) => {
 	for (let i = 1; i <= n; i++) { // For rows
 		let log = '';
 		for (let j = 1; j <= n; j++) { // For columns
@@ -11,9 +11,24 @@ const steps = (n) => {
 		}
 		// Log when row ready with all columns means when we finish for columns loop
 		console.log(log);	
+	}	
+}
+
+// Solution 2
+//steps(3)
+const steps2 = (n, row = 0, stair = '') => {
+	// Set base case
+	if (n === row) return;
+
+	if (n === stair.length) {
+		console.log(stair);
+		return steps2(n, row + 1);
 	}
-	
+
+	const add = (stair.length <= row) ? '#' : ' ';
+
+	steps2(n, row, stair + add);
 }
 
 
-module.exports = steps;
+module.exports = steps2;
