63dc817f34ad493ead64ef05773bcf8e69594fda
Navdeep Singh <navdeep.er@gmail.com>
2018-10-19 13:47:34 +0530

    Understanding complex but simple fibonacci

--- 014-fibonacci/index.js
+++ 014-fibonacci/index.js
@@ -1,6 +1,6 @@
 // Solution 1
 // Iteration and Linear Runtime Complexity 
-const fibonacci = (n) => {
+const fibonacci1 = (n) => {
 	let results = [0,1];
 	for(let i = 2; i <= n; i++ ){
 		results.push((results[results.length - 1]) + (results[results.length - 2]));
@@ -8,4 +8,13 @@ const fibonacci = (n) => {
 	return results[results.length - 1];
 }
 
-module.exports = fibonacci;
+// Solution 2
+// Recursive Solution
+const fibonacci2 = (n) => {
+	// Set Base Case
+	if (n < 2) return n;
+
+	return fibonacci2(n - 1) + fibonacci2(n-2);
+}
+
+module.exports = fibonacci2;
