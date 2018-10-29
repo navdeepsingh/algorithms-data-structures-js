3d967ec1e60af4e0b64a9294fd4a63037dce392e
Navdeep Singh <navdeep.er@gmail.com>
2018-10-29 13:35:45 +0530

    Weave the queues

---
+++ 016-weave/index.js
@@ -0,0 +1,17 @@
+const Queue = require('./queue');
+
+const weave = (sourceOne, sourceTwo) => {
+	const q =  new Queue();
+
+	while (sourceOne.peek() && sourceTwo.peek()) {
+		if (sourceOne.peek()) {
+			q.add(sourceOne.remove());
+		}
+		if (sourceTwo.peek()) {
+			q.add(sourceTwo.remove());
+		}		
+	}
+	return q;
+}
+
+module.exports = weave;
